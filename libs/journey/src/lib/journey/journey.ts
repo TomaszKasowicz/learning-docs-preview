aa86229309c96c34edc899fa68642dbe4387fdf3
Tomasz Kasowicz <tomasz.kasowicz@gmail.com>
2025-10-08 09:43:21 +0200

    chore: add journey lib

---
+++ libs/journey/src/lib/journey/journey.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-journey',
+  imports: [],
+  template: `<p>Journey works!</p>`,
+  styles: ``,
+})
+export class Journey {}
