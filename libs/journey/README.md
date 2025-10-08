36a6dedb8ad30bbb36d79b3ac7e2ff848f690784
Tomasz Kasowicz <TomaszKasowicz@users.noreply.github.com>
2025-10-08 10:58:33 +0200

    ci: add docs preview (#1)
    * chore: add journey lib
    
    * chore: add compodoc
    
    * ci: add docs preview
    
    * ci: update pr workflow

---
+++ libs/journey/README.md
@@ -0,0 +1,7 @@
+# journey
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test journey` to execute the unit tests.
---
+++ libs/journey/src/lib/journey/journey.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+/**
+ * Journey component
+ * @description Journey component
+ * @example
+ * <lib-journey></lib-journey>
+ */
+@Component({
+  selector: 'lib-journey',
+  imports: [],
+  template: `<p>Journey works!</p>`,
+  styles: ``,
+})
+export class Journey {}
